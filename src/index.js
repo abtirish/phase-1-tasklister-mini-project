11f68856b9c7fc52fa500e5544ba3b116b47f27b
abtirish <abtirish@gmail.com>
2023-10-29 15:43:45 -0600

    finish phase 1 tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  //Generates a submit event on the Create New Task button 
+    document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    //Since the id had dashes, needed to use querySelector to find. 
+    AddToDo(e.target.querySelector('#new-task-description').value)
+    })
+
+})
+
+// Create list (li) element with an 'x' button of inputs added to the list
+function AddToDo(todo) {
+  let list = document.createElement('LI');
+  let button = document.createElement('button')
+  button.addEventListener('click', handleDelete) 
+  button.textContent = ' x '
+  list.textContent = `${todo}  `
+  list.appendChild(button)
+  document.querySelector('#tasks').appendChild(list)
+}
+
+//When clicking the 'x' button, the button and text should be removed 
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
